cae43b8b0cb24a672db8dc9a10d9bf10c4737e7d
Javier-deaz <javierdeazep@gmail.com>
2022-03-08 15:02:58 -0500

    Agregando función cambio de estado del boton ingresar Login

--- js/funciones.js
+++ js/funciones.js
@@ -1,15 +1,22 @@
+// Función para cambiar estado del boton ingresar
+let inputTexto = document.querySelectorAll(".input-texto");
+inputTexto[1].addEventListener("keyup", () => {
+    if (inputTexto[0].value != "" && inputTexto[1].value != "") {
+        $("#btn-ingresar").removeClass('boton-desactivado-bg');
+        $("#btn-ingresar").addClass('boton-activo-bg');
+    } else {
+        $("#btn-ingresar").addClass('boton-desactivado-bg');
+    }
+})
 // Función para ver contraseña del login
-function verPass(){
-	var x = document.getElementById("input-pass");
-	var y = document.querySelector(".show-password");
-	if(x.type == "password"){
-		x.type = "text";
-		y.classList.remove('fa-eye-slash');
-	}else{
-		x.type = "password";
-		y.classList.toggle("fa-eye-slash");
-	}
+function verPass() {
+    var x = document.getElementById("input-pass");
+    var y = document.querySelector(".show-password");
+    if (x.type == "password") {
+        x.type = "text";
+        y.classList.remove('fa-eye-slash');
+    } else {
+        x.type = "password";
+        y.classList.toggle("fa-eye-slash");
+    }
 }
-
-
-
